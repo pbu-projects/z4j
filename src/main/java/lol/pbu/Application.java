a64c817adf0d2b4cc7a388537ca6b88c5e342fab
jonathan zollinger <jonathan.zollinger@outlook.com>
2025-10-03 22:07:55 -0600

    refactor: cleanup unused files

--- src/main/java/lol/pbu/Application.java
+++
@@ -1,9 +0,0 @@
-package lol.pbu;
-import io.micronaut.runtime.Micronaut;
-
-public class Application {
-
-    public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
-    }
-}
