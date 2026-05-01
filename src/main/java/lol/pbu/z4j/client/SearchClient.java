ac8fec96c4f315ce112e23576cddc7476b929a48
jonathan zollinger <jonathan.zollinger@outlook.com>
2026-05-01 16:31:30 -0600

    docs: correct export javadocs

--- src/main/java/lol/pbu/z4j/client/SearchClient.java
+++ src/main/java/lol/pbu/z4j/client/SearchClient.java
@@ -56,7 +56,34 @@ public interface SearchClient {
 
     /**
      * <h1>{@summary Export Search Results}</h1>
-     * <p>Exports a set of results. See <a href='https://developer.zendesk.com/api-reference/ticketing/ticket-management/search/#query-syntax'>Query syntax</a> for the syntax of the {@code query} parameter.</p> <p>Use this endpoint for search queries that will return more than 1000 results. The result set is ordered only by the {@code created_at} attribute.</p> <p>The search only returns results of a single object type. The following object types are supported: ticket, organization, user, or group.</p> <p>You must specify the type in the {@code filter[type]} parameter. Searches with type in the query string will result in an error.</p> Allowed for Agents <h4>Pagination</h4> <ul>     <li>Cursor pagination</li> </ul> <p>See <a href='https://developer.zendesk.com/api-reference/introduction/pagination/'>Pagination</a>.</p> <p>Returns a maximum of 1000 records per page. The number of results shown in a page is determined by the {@code page[size]} parameter.</p> <p><strong>Note</strong>: You may experience a speed reduction or a timeout if you request 1000 results per page and you have many archived tickets in the results. Try reducing the number of results per page. We recommend 100 results per page.</p> <p>The cursor specified by the {@code after_cursor} property in a response expires after one hour.</p> <p>For more information on cursor-based pagination, see the following articles:</p> <ul>     <li><a href='https://developer.zendesk.com/documentation/developer-tools/pagination/comparing-cursor-pagination-and-offset-pagination'>Comparing cursor pagination and offset pagination</a></li>     <li><a href='https://developer.zendesk.com/documentation/developer-tools/pagination/paginating-through-lists-using-cursor-pagination'>Paginating through lists using cursor pagination</a></li> </ul> <h4>Export Search Results Limits</h4> <p>This API endpoint is rate-limited to 100 requests per minute per account. The limit also counts towards the global API rate limit.</p>
+     * <p>Exports a set of results. See <a
+     *         href='https://developer.zendesk.com/api-reference/ticketing/ticket-management/search/#query-syntax'>Query
+     *     syntax</a> for the syntax of the {@code query} parameter.</p> <p>Use this endpoint for search queries that will
+     *     return more than 1000 results. The result set is ordered only by the {@code created_at} attribute.</p> <p>The search
+     *     only returns results of a single object type. The following object types are supported: ticket, organization, user,
+     *     or group.</p> <p>You must specify the type in the {@code filter[type]} parameter. Searches with type in the query
+     *     string will result in an error.</p> Allowed for Agents <h4>Pagination</h4>
+     * <ul>
+     *     <li>Cursor
+     *         pagination
+     *     </li>
+     * </ul> <p>See <a href='https://developer.zendesk.com/api-reference/introduction/pagination/'>Pagination</a>.</p> <p>
+     *     Returns a maximum of 1000 records per page. The number of results shown in a page is determined by the {@code
+     *     page[size]} parameter.</p> <p><strong>Note</strong>: You may experience a speed reduction or a timeout if you
+     *     request 1000 results per page and you have many archived tickets in the results. Try reducing the number of results
+     *     per page. We recommend 100 results per page.</p> <p>The cursor specified by the {@code after_cursor} property in a
+     *     response expires after one hour.</p> <p>For more information on cursor-based pagination, see the following
+     *     articles:</p>
+     * <ul>
+     *     <li>
+     *         <a href='https://developer.zendesk.com/documentation/developer-tools/pagination/comparing-cursor-pagination-and-offset-pagination'>Comparing
+     *             cursor pagination and offset pagination</a></li>
+     *     <li>
+     *         <a href='https://developer.zendesk.com/documentation/developer-tools/pagination/paginating-through-lists-using-cursor-pagination'>Paginating
+     *             through lists using cursor pagination</a></li>
+     * </ul> <h4>Export Search Results Limits</h4> <p>This API endpoint is rate-limited to 100 requests per minute per account.
+     *     The limit also counts towards the global API rate
+     *     limit.</p>
      *
      * @param query      Returns the search results. See <a href='https://developer.zendesk.com/api-reference/ticketing/ticket-management/search/#query-syntax'>Query syntax</a> for details on the {@code query} parameter. For details on the query syntax, see the <a href='https://support.zendesk.com/hc/en-us/articles/203663226'>Zendesk Support search reference</a>. (required)
      * @param pageSize   The number of results shown in a page. (required)
