7d5982fa9134d885adda85e7c5201509de09fa1a
jonathan zollinger <jonathan.zollinger@outlook.com>
2026-05-08 16:39:48 -0600

    docs: add docs to SearchService
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/lol/pbu/z4j/SearchService.java
+++ src/main/java/lol/pbu/z4j/SearchService.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2026 Peanut Butter Unicorn, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package lol.pbu.z4j;
 
 import jakarta.inject.Inject;
@@ -19,10 +34,25 @@ public class SearchService {
     @Inject
     SearchClient searchClient;
 
+    /**
+     * Searches for tickets using the provided query string with a default page size of 100.
+     *
+     * @param query The search query string. Note: 'type:ticket' is automatically prepended.
+     * @return A {@link Flux} emitting the {@link Ticket} results found.
+     * @see #getTickets(String, int)
+     */
     public Flux<Ticket> getTickets(String query) {
         return getTickets(query, 100);
     }
 
+
+    /**
+     * Searches for tickets using the provided query string and page size.
+     *
+     * @param query    The search query string. Note: 'type:ticket' is automatically prepended.
+     * @param pageSize The number of results to return per page.
+     * @return A {@link Flux} emitting the {@link Ticket} results found.
+     */
     public Flux<Ticket> getTickets(String query, int pageSize) {
         String ticketPreface = "type:ticket";
         query = query.replace(ticketPreface + " ", "").replace(ticketPreface, "");
